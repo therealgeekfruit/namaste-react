67b20edb432431f2c5bca186a9bcf467d3b4c8cf
Sudeep Rao <sudeep.rao619@gmail.com>
2024-01-11 20:53:26 +0530

    hello world from React

---
+++ index.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("div", {}, [
+  React.createElement("h1", {}, "Hello World from React!"),
+  React.createElement("h2", { className: "h2class" }, "H2 line in react"),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
