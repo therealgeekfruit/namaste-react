ae14d4812c9058f1830341e8f297f90aa90b6f97
Sudeep Rao <sudeep.rao619@gmail.com>
2024-01-18 13:32:09 +0530

    react with parcel bundler

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", {}, [
   React.createElement("h1", {}, "Hello World from React!"),
   React.createElement("h2", { className: "h2class" }, "H2 line in react"),
